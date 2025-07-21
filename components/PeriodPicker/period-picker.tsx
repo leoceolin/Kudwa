5921c1d7490158eb2f5a6dbe075baf516e68b8ce
leonel <leonell_ceolin@hotmail.com>
2025-07-21 17:51:30 -0300

    period picker component

---
+++ components/PeriodPicker/period-picker.tsx
@@ -0,0 +1,44 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { useAppDispatch, useAppSelector } from '@/store/hooks'
+import { setViewMode } from '@/store/slices/viewMode'
+import { VIEW_MODES } from '@/consts'
+
+export function PeriodPicker() {
+  const dispatch = useAppDispatch()
+  const viewMode = useAppSelector((state) => state.viewMode)
+
+  return (
+    <div className="flex items-center gap-4 justify-center">
+      Select a period
+      <Select
+        value={viewMode}
+        onValueChange={(val) => {
+          setLoading(true)
+          dispatch(setViewMode(val as 'monthly' | 'quarterly' | 'yearly'))
+        }}
+      >
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a period" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Period</SelectLabel>
+            {VIEW_MODES.map((mode) => (
+              <SelectItem key={mode.value} value={mode.value}>
+                {mode.label}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
