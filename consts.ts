5a0a2d206a375be752afcd39aab63e89b64064aa
leonel <leonell_ceolin@hotmail.com>
2025-07-21 17:48:24 -0300

    create global const to be acessible in all app

---
+++ consts.ts
@@ -0,0 +1,5 @@
+export const VIEW_MODES = [
+  { label: 'Monthly', value: 'monthly', kpiLabel: 'Month-over-Month' },
+  { label: 'Quarterly', value: 'quarterly', kpiLabel: 'Quarter-over-Quarter' },
+  { label: 'Yearly', value: 'yearly', kpiLabel: 'Year-over-Year' }
+]
